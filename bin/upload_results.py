f6dad28045cc6a3f738b570ee40555c75b3f3db4
Zheng Meyer <meyer@astron.nl>
2020-02-03 14:15:27 +0100

    create directory for targ1 output

--- bin/upload_results.py
+++ bin/upload_results.py
@@ -11,12 +11,17 @@ token = os.environ["TOKEN"]
 pc = PicasCred()
 upload_data = get_token_field(token, 'upload', pc)
 context = get_token_field(token, 'config.json', pc)
+results_dir = get_token_field(token, 'RESULTS_DIR', pc)
+obsid = get_token_field(token, 'OBSID', pc)
+
 if not upload_data:
     sys.exit()
 context['upload'] = upload_data
 uberftp_exists = subprocess.Popen(['which','uberftp'], stdout=subprocess.PIPE).communicate()[0]
 
 if uberftp_exists:
+    # create directory
+    subprocess.Popen(['uberftp', '-mkdir', results_dir+'/'+obsid], stdout=subprocess.PIPE)
     results_uploader = GSIUploader(context)
 else:
     results_uploader = uploader(context)
