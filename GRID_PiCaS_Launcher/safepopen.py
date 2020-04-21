7b0ca98f0cc76b4e47f48ed2611694353e344744
Frits Sweijen <fritssweijen@hotmail.com>
2020-04-21 14:28:42 +0200

    Fix pipeline status update issue for py2

--- GRID_PiCaS_Launcher/safepopen.py
+++ GRID_PiCaS_Launcher/safepopen.py
@@ -1,10 +1,10 @@
 from subprocess import Popen
 import sys
 
-class SafePopen(object):                                                                                         
+class SafePopen(Popen):                                                                                         
     def __init__(self, *popen_args, **popen_kwargs):
         if sys.version_info.major ==2:
-            return Popen.__init__(*popen_args, **popen_kwargs)
+            return super(SafePopen, self).__init__(*popen_args, **popen_kwargs)
         if sys.version_info.major == 3 : 
             popen_kwargs['encoding'] = 'utf8'
         return Popen.__init__(*popen_args, **popen_kwargs)
